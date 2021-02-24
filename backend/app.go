a2e1d4bd60ab2276530065b036c61f46d147483c
Breno Silva <brenoe133@gmail.com>
2021-02-24 17:29:07 -0300

    Create a endpoint to return languages definitions

--- backend/app.go
+++ backend/app.go
@@ -1,13 +1,40 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+
+func OpenJson(fileName string) []map[string]interface{}{
+
+    jsonFile, err := os.Open(fileName)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    var result []map[string]interface{}
+
+    json.Unmarshal([]byte(byteValue), &result)
+
+    return result
+}
+
+func LanguagesInfo(context *gin.Context) {
+	content := OpenJson("languages_definition.json")
+	context.JSON(200, gin.H{
+		"data": content,
+	})
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	router := gin.Default()
+	router.GET("/languages/info", LanguagesInfo)
+	router.Run()
 }
