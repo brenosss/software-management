414f1efd1f3fe1003a2ea812c07eb277b7e5f37b
Breno Silva <brenoe133@gmail.com>
2021-04-21 14:28:24 -0300

    Create functions to save project and tasks

--- backend/src/database/queries/project.go
+++ backend/src/database/queries/project.go
@@ -1,9 +1,62 @@
 package queries
 
 import (
+	"backend/src/database"
 	"backend/src/entities"
+	"log"
 )
 
 func CreateProject() entities.Project {
-	return entities.NewRandonProject()
+	createProjectQuery := `
+		INSERT INTO project (
+			project_Id,
+			name
+		) values(?,?)
+	`
+	db := database.GetConnection()
+	project := entities.NewRandonProject()
+	stmt, err := db.Prepare(createProjectQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(project.ProjectId, project.Name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("New person project")
+	for _, task := range project.Tasks {
+		log.Print(task)
+		CreateTask(task)
+	}
+	return project
+}
+
+func CreateTask(task entities.Task) entities.Task {
+	createTaskQuery := `
+	INSERT INTO task (
+		task_id,
+		project_id,
+		name,
+		type,
+		description,
+		complexity,
+		duration,
+		progress
+	) values(
+		:task_id,
+		:project_id,
+		:name,
+		:type,
+		:description,
+		:complexity,
+		:duration,
+		:progress
+	)
+	`
+	db := database.GetConnection()
+	_, err := db.NamedExec(createTaskQuery, task)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return task
 }
